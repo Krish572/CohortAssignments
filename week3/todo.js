7345e262be4b3dd3d41a450002726dbd39694438
Krishna <sushant05072@gmail.com>
2025-08-17 22:59:56 +0530

    add crd operation in todo

--- week3/todo.js
+++ week3/todo.js
@@ -1,9 +1,20 @@
 let inputEl = document.querySelector("input");
 let divEl = document.querySelector("#divEl");
 
+let c = 1;
+
 const addTodo = () => {
     console.log(inputEl.value);
     let child = document.createElement("div");
-    child.innerHTML = `<span>${inputEl.value}<span> <button>Delete</button>`;
+    child.setAttribute("class", "todo");
+    child.setAttribute("id", `todo-${c}`);
+    child.innerHTML = `<span>${inputEl.value}</span> <button onclick={deleteTodo("todo-${c}")}>Delete</button>`;
     divEl.appendChild(child);
+    c++;
+}
+
+const deleteTodo = (id) => {
+    console.log("Deleted");
+    const deleteEl = document.getElementById(id);
+    deleteEl.parentNode.removeChild(deleteEl);
 }
